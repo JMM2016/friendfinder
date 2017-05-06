60a2d13571b3aaa6d5e0364bcecbe3f5cc2ffe72
jmm2016 <sus.magallanes@gmail.com>
2017-05-05 23:42:49 -0500

    more done

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,20 +1,3 @@
-const friends = [
-  {
-    "name":"Ahmed",
-    "photo":"https://upload.wikimedia.org/wikipedia/commons/3/39/AlthepalHappyface.png",
-    "scores":[
-      5,
-      1,
-      4,
-      4,
-      5,
-      1,
-      2,
-      5,
-      4,
-      1
-    ]
-  }
-]
+const friends = []
 
 module.exports = friends
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -15,13 +15,7 @@ module.exports = function(svr) {
   // Route that gets friend data from page
   svr.post("/api/friends", (req, res) => {
 
-    // I guess body will have the name,photo ,scores[]
-    // ready to go into friendsData ???
-    console.log("r.body= " + JSON.stringify(req.body))
-
     let bestMatch
-
-    console.log("fData: " + friendsData.length )
 
     if (friendsData.length === 0) {
       bestMatch = {
@@ -29,7 +23,7 @@ module.exports = function(svr) {
         "photo":"http://findicons.com/files/icons/350/aqua_smiles/128/sad.png"
       }
     } else {
-
+      bestMatch = friendsData[0]
       let highScore = -1
 
       // compare the new friend to all the other friends
@@ -37,26 +31,20 @@ module.exports = function(svr) {
 
         // grab first friend
         let tempMatch = friendsData[i]
-        bestMatch = tempMatch
         let sum = 0
 
         for (let j = 0; j < tempMatch.scores.length; j++) {
-          console.log (req.body.scores[j] - tempMatch.scores[j])
-          console.log(Math.abs(req.body.scores[j] - tempMatch.scores[j]))
           sum +=  Math.abs(req.body.scores[j] - tempMatch.scores[j])
-          console.log("inner sum: " + sum)
         }
 
-        if(sum > highScore){
+        if (sum > highScore) {
           highScore = sum
           bestMatch = tempMatch
         }
-        console.log("outer sum: " + sum)
       }
     }
 
     friendsData.push(req.body)
-    console.log("bM: " + bestMatch)
     res.send(bestMatch)
   })
 }
